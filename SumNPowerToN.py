911343317894a21c2801642dbd26053e6ddd4b7a
satyaupendra <satyaupendra.devabhaktineni9@gmail.com>
2022-08-10 18:10:23 -0500

    updated SumNPowerN added Print Statement

--- SumNPowerToN.py
+++ SumNPowerToN.py
@@ -6,5 +6,6 @@ for i in range(1,n+1):
     temp = powerOfNumber(i,i)
     print("{n} power {n} is {temp}".format(n=i,temp=temp))
     sum = sum +temp
+    print("Sum of Nth powers of first {e} numbers is : {sum}".format(e=i,sum=sum))
 print("Sum of Series N to Power N: ",sum)
     
