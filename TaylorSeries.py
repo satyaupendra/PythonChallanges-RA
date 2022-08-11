791de8a6163772b84defe49771f41846c724fce2
satyaupendra <satyaupendra.devabhaktineni9@gmail.com>
2022-08-10 23:01:43 -0500

    RA - Python Challanges

--- TaylorSeries.py
+++ TaylorSeries.py
@@ -5,7 +5,7 @@ for i in range(n+1):
   for j in range(1,i+1,1):
     temp = temp * j
   sum = sum + 1.0/temp
-  print("Sum of after first {i} terms is {sum}".format(i=i+1, sum=format(sum,".50f")))
+  print("Sum of after first {i} terms is {sum}".format(i=i+1, sum=format(sum,".6f")))
   print("Latest term is {temp}".format(temp=format(1/temp, ".5f")))
 
   # I have obsereved that after the sum of first 10 numbers the approximate value of taylor series is 2.71828,
